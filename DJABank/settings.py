8ce7f8b114aad0c2d29f1fbf2d47ed5784083971
SopuruchiCode <utahemmanuel452@gmail.com>
2026-03-22 17:35:41 +0100

    debug true

--- DJABank/settings.py
+++ DJABank/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['192.168.0.1', 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ["bank-app-t2e1.onrender.com"]
+ALLOWED_HOSTS = ["bank-app-t2e1.onrender.com", "127.0.0.1"]
 
 
 # Application definition
